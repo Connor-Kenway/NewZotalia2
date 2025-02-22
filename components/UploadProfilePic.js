894f3da7f8b6eac1f14bb375776d44d4820daba8
Ryan Eurich <reurich@uci.edu>
2025-02-21 21:38:12 -0800

    deleted file

--- components/UploadProfilePic.js
+++
@@ -1,3 +0,0 @@
-import React, { useState } from "react";
-import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
-import * as ImagePicker from "expo-image-picker"; // For picking images from device library
